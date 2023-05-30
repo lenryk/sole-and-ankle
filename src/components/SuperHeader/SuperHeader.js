b76bdee0771fe28db0d7eae711a0824a93aa516f
Sam <lenry101@gmail.com>
2023-05-30 23:04:58 +0100

    exercise 1: superheader

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -23,13 +23,19 @@ const SuperHeader = () => {
 };
 
 const Wrapper = styled.div`
+  display: flex;
+  gap: 24px;
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  padding: 0 32px;
+  min-height: 40px;
+  align-items: center;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  margin-right: auto;
 `;
 
 const HelpLink = styled.a`
